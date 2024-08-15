180c7901fefb0ee7534ca00daaf0ac2b5713ac03
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-15 22:31:25 +0300

    Adds all files

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,19 @@
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
+  it("returns the current year for a person born in year 0", () => {
+    // Arrange
+    const birthYear = 0;
+  
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+  
+    // Assert
+    expect(ageOfPerson).toBe(2022);
   });
+  // it("returns the age of a person based on the year of birth", () => {
+  //   const birthYear = 1984;
+  //   const ageOfPerson = currentAgeForBirthYear(birthYear);
+  //   expect(ageOfPerson).toBe(38);
+  // });
 });
